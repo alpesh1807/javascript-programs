3bdc7348ab2c344907492dc6a98639764687ada4
alpesh1807 <alpesh.shah1807@gmail.com>
2024-07-31 07:19:18 -0700

    01-variableAssign
    01-variableAssign

---
+++ 01-variableAssign.js
@@ -0,0 +1,34 @@
+console.log("============= Name ===============");
+var myFullName ;
+console.log("My Full Name is:", myFullName);
+var myFullName = "Alpesh Shah";
+console.log("My Full Name is: ", myFullName);
+var myFullName = "Alpesh V. Shah";
+console.log("My Full Name is: ", myFullName);
+
+console.log("============= My love ===============");
+var myLove = 'I love only JS';
+console.log("My love  is:", myLove);
+
+console.log("============= Address ===============");
+var firstName = "Alpesh";
+console.log("First Name :", firstName);
+
+var lastName = "Shah";
+console.log("Last Name :", lastName);
+
+var age = "35";
+console.log("Age :", age);
+
+var address = "Jawahar Ward, Pandhurna, M.P.";
+console.log("Address :", address);
+
+var pincode = "480334";
+console.log("Pincode :", pincode);
+
+var gender = "Male";
+console.log("Gender :", gender);
+
+var isMarried = "False";
+console.log("Is Married :", isMarried);
+
