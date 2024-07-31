77b2ee8162dabcb7575cfe650cda42f0a0d3da31
alpesh1807 <alpesh.shah1807@gmail.com>
2024-07-30 19:23:31 -0700

    javascript variables
    javascript variables

---
+++ 01-variable-assignment.js
@@ -0,0 +1,14 @@
+var myCityName = "My City Name : Pandhurna";
+console.log(myCityName);
+
+var myBestFriendName = "My Best Friend Name : Deepak";
+console.log(myBestFriendName);
+
+var myCountryName = "My Country Name : India";
+console.log(myCountryName);
+
+var mySchoolName = "My School Name : Govt. Hr.S.S. Pandhurna";
+console.log(mySchoolName);
+
+var myCollegeName = "My College Name : SATI, Vidisha";
+console.log(myCollegeName);
---
+++ test.js
@@ -0,0 +1,2 @@
+
+console.log("Alpesh Shah");
