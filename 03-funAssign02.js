f7c1209bf2ae2616440e4a1c57275364ef98688a
alpesh1807 <alpesh.shah1807@gmail.com>
2024-08-02 08:45:36 -0700

    03-funAssign02
    03-funAssign02

---
+++ 03-funAssign02.js
@@ -0,0 +1,7 @@
+console.log("============= Bank Details ===============");
+function bankDetails(bankName, accountNumber, location, pincode) {
+    console.log("Bank Details is:", bankName, accountNumber, location, pincode);   
+}
+bankDetails ("CITI Bank ", 3456782345, " Pune ", 431202);
+bankDetails ("AXIS Bank ", 7856782345, " Mumbai ", 441202);
+bankDetails ("HDFC Bank ", 8956782345, "Pune ", 631202, "open");
