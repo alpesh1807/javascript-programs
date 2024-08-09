f963b0dc7691284299e1f07feaec92fc4c332e7b
alpesh1807 <alpesh.shah1807@gmail.com>
2024-08-09 10:06:07 -0700

    06-for-assignment01.js
    06-for-assignment01.js

---
+++ 06-for-assignment01.js
@@ -0,0 +1,34 @@
+console.log(`============== Print Numbers from 5 to 15 by incrementing 1 ==================`);
+for (let index = 5; index <=15; index++) {
+    console.log(index);  
+}
+
+console.log(`============== Print numbers from 50 to 40 decrementing by 1 ==================`);
+for (let index = 50; index>=40 ; index=index-1) {
+    console.log(index);
+}
+
+console.log(`============== First 15 odd Number ==================`);
+for (let index = 1; index <=30; index=index+2) {
+    console.log(index);  
+}
+
+console.log(`============== First 10 Even Number ==================`);
+for (let index = 2; index <=20; index=index+2) {
+    console.log(index);  
+}
+
+console.log(`============= Print Table of 5 ===================`);
+for (let index = 5; index<=50 ; index=index+5) {
+    console.log(index);
+}
+
+console.log(`============== Print Table of 10 ==================`);
+for (let index = 10; index<=100 ; index=index+10) {
+    console.log(index);
+}
+
+console.log(`============== Print Table of 10 in reverse order ==================`);
+for (let index = 100; index>=10 ; index=index-10) {
+    console.log(index);
+}
